28aea96434406eb4c3c432529bd3f609d649df85
Rohan Tomar <rstomar710@gmail.com>
2022-01-27 22:07:26 +0530

    make it better

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ app.get("/",async(req,res)=>{
 })
 
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+const PORT = server.listen(process.env.PORT || 3000);
+app.listen(PORT);
