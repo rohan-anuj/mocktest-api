6601c0812dd1bd48fccf0f91b5365df0404c85f6
Rohan Tomar <rstomar710@gmail.com>
2022-01-31 18:27:35 +0530

    new database storage

--- Routes/userLoged.js
+++ Routes/userLoged.js
@@ -7,13 +7,12 @@ route.post("/",async(req,res)=>{
     console.log(req.body)
     const db=await sqlite.open({filename:"database.json",driver:sqlite3.Database})
     const data=await (await db).get("select * from user where email=?",req.body.email)
-    console.log(data)
+
     if(!data){
         res.status(400).json("user doesn't exist!")
     }
     else{
-        console.log(data,"l")
-        data.password===req.body.password?res.json("User Authenticated!"):res.status(400).json("user Unauthenticated")
+    data.password===req.body.password?res.json({id:data.id}):res.status(400).json("user Unauthenticated")
       }
 })
 
