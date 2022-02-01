1f2669b3918d20c1ccdaea1fa76a9ff962fb7e1f
Rohan Tomar <rstomar710@gmail.com>
2022-02-01 15:50:05 +0530

    .env file & jwt added

--- Routes/userLoged.js
+++ Routes/userLoged.js
@@ -2,17 +2,21 @@ const sqlite=require("sqlite")
 const sqlite3=require("sqlite3")
 const express=require("express")
 const route=express.Router()
+const jwt=require("jsonwebtoken")
+require("dotenv").config()
 
 route.post("/",async(req,res)=>{
+ 
     console.log(req.body)
     const db=await sqlite.open({filename:"database.json",driver:sqlite3.Database})
     const data=await (await db).get("select * from user where email=?",req.body.email)
+    const token=await jwt.sign({email:req.body.email},process.env.key)
 
     if(!data){
         res.status(404).json("user doesn't exist!")
     }
     else{
-    data.password===req.body.password?res.json({id:data.id}):res.status(400).json("user Unauthenticated")
+    data.password===req.body.password?res.json({token:token,id:data.id}):res.status(400).json("user Unauthenticated")
       }
 })
 
