a059a0e50bb6d32ab2231cf57164c80698bb6c4f
Rohan Tomar <rstomar710@gmail.com>
2022-02-04 12:36:32 +0530

    change in route

--- migrations/001-database.sql
+++ migrations/001-database.sql
@@ -3,8 +3,11 @@ CREATE TABLE user(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT,
     password TEXT,
-    test INTEGER 
-
+    test INTEGER DEFAULT 0,
+    tallyscore INTEGER DEFAULT 0,
+    cccscore INTEGER DEFAULT 0,
+    dtpscore INTEGER DEFAULT 0,
+    excelscore INTEGER DEFAULT 0
 
 )
 
