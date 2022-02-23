6de00b2c00ac4516c7efc3f8191ad7b490905f49
Rohan Tomar <rstomar710@gmail.com>
2022-02-23 21:26:07 +0530

    marks entry pushed

--- Routes/marksEntered.js
+++ Routes/marksEntered.js
@@ -2,13 +2,15 @@ const sqlite=require("sqlite")
 const sqlite3=require("sqlite3")
 const express=require("express")
 const route=express.Router()
+const jwt=require("jsonwebtoken")
+require("dotenv").config()
 
 
 route.put("/",async(req,res)=>{
     const db=await sqlite.open({filename:"database.json",driver:sqlite3.Database})
     const success=  await (await db).run(`update user set "${req.body.topic}"="${req.body.score}" where email="${req.body.email}"`)
-
-    success?res.json("Successfull"):res.status(400).json("something went wrong!")
+    const token=await jwt.sign({email:req.body.email},process.env.key,{expiresIn:"1h"})
+    success?res.json(token):res.status(400).json("something went wrong!")
 
 })
 
